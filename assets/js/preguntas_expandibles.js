66810a24abdd66053f7dbdc38f1f07ae561bb99b
Hectorreto <hdeandaf@hotmail.com>
2021-05-02 19:34:53 -0500

    JQuery para animaciones al hacer clic en las preguntas

--- assets/js/preguntas_expandibles.js
+++ assets/js/preguntas_expandibles.js
@@ -1,7 +1,7 @@
 for (let pregunta of document.getElementsByClassName("expandible")) {
-    let respuesta = pregunta.getElementsByClassName("oculto")[0];
+    let respuesta = pregunta.querySelector("div.oculto");
     pregunta.onclick = function() {
-        respuesta.classList.toggle("oculto");
+        // respuesta.classList.toggle("oculto");
+        $(respuesta).slideToggle();
     };
 }
-
