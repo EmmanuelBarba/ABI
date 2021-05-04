a3cac434ff7d24fd5195e88da56d91c7b7e2dd3f
The-Gae-Bolg <65456792+The-Gae-Bolg@users.noreply.github.com>
2021-05-03 22:49:45 -0500

    js

--- assets/js/preguntas_expandibles.js
+++ assets/js/preguntas_expandibles.js
@@ -1,7 +1,11 @@
-for (let pregunta of document.getElementsByClassName("expandible")) {
-    let respuesta = pregunta.querySelector("div.oculto");
-    pregunta.onclick = function() {
+$(function () {
+    
+    
+    for (let pregunta of document.getElementsByClassName("expandible")) {
+        let respuesta = pregunta.querySelector("div.oculto");
+        pregunta.onclick = function() {
         // respuesta.classList.toggle("oculto");
         $(respuesta).slideToggle();
-    };
-}
+        };
+    }
+});
