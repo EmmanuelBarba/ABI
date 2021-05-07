574e95b883e8829f2fb57334995d67cb66e58157
Hectorreto <hdeandaf@hotmail.com>
2021-05-07 10:35:15 -0500

    CSS de las listas de precios en las preguntas

--- assets/js/preguntas_expandibles.js
+++ assets/js/preguntas_expandibles.js
@@ -4,18 +4,18 @@ $(function () {
     for (let pregunta of document.getElementsByClassName("expandible")) {
         let respuesta = pregunta.querySelector("div.oculto");
         pregunta.onclick = function() {
-        // respuesta.classList.toggle("oculto");
-        $(respuesta).slideToggle();
+            // respuesta.classList.toggle("oculto");
+            $(respuesta).slideToggle();
         };
     }
     
     $('#myDiv').floatingWhatsApp({
         phone: '523781080994',
-        popupMessage: 'En qué podemos ayudarte?',
+        popupMessage: '¿En qué podemos ayudarte?',
         message: "",
         showPopup: true,
         showOnIE: false,
-        headerTitle: 'Hola!',
+        headerTitle: 'Contacto vía WhatsApp',
         position: "right"
     });
 });
