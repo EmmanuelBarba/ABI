fd4c028e2c216556db5da76ab12c84f0a636444c
The-Gae-Bolg <65456792+The-Gae-Bolg@users.noreply.github.com>
2021-05-28 18:32:42 -0500

    error de archivo

--- assets/js/preguntas_expandibles.js
+++ assets/js/preguntas_expandibles.js
@@ -1,13 +1,26 @@
 $(function () {
     
+    var r=["rotate(180deg)","rotate(90deg)"]
     
     for (let pregunta of document.getElementsByClassName("expandible")) {
         let respuesta = pregunta.querySelector("div.oculto");
+        let masMenos = pregunta.querySelector("p.masMenos")
         pregunta.onclick = function() {
             // respuesta.classList.toggle("oculto");
             $(respuesta).slideToggle();
+            if( masMenos.innerHTML=="+"){
+                masMenos.innerHTML="-";
+                masMenos.style.transition="all .5s"
+                masMenos.style.transform=r[0];
+            }else{
+                masMenos.innerHTML="+";
+                masMenos.style.transition="all .5s"
+                masMenos.style.transform=r[1];
+
+            }
         };
     }
+    
     
     $('#myDiv').floatingWhatsApp({
         phone: '523781080994',
